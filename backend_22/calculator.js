35df4f67f0aded23ffae79c0905e6b34ff44a040
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-27 00:32:28 +0530

    file added in reponse command through sendFile

--- backend_22/calculator.js
+++ backend_22/calculator.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/" , function(request , response){
-    response.send("<h1>Hello</h1>");
+    response.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000 , function(){console.log("server started on port 3000");});
