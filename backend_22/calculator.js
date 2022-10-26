193981653f8762c11557cd769a035bd00826cdb7
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-26 19:24:59 +0530

    setup of express and reuiring express server and routing and setting server at 3000 port

---
+++ backend_22/calculator.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/" , function(request , response){
+    response.send("<h1>Hello</h1>");
+});
+
+app.listen(3000 , function(){console.log("server started on port 3000");});
