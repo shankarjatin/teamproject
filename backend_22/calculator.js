4b79f4601b31d29f1f8c9783851caee820a6de19
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-27 02:50:36 +0530

    post reques but error in getting post

--- backend_22/calculator.js
+++ backend_22/calculator.js
@@ -5,4 +5,10 @@ app.get("/" , function(request , response){
     response.sendFile(__dirname + "/index.html");
 });
 
+app.post("/" , function(request , response){
+    response.send("thanks for posting that");
+});
+
+
+
 app.listen(3000 , function(){console.log("server started on port 3000");});
