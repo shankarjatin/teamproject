3f801213030bf953ca965aaa60bee643bb3a326b
Jatin shankar <shankarjatin1005@gmail.com>
2022-10-27 02:59:07 +0530

    issue solved and now post is also working

--- backend_22/calculator.js
+++ backend_22/calculator.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}))
 
-app.get("/" , function(request , response){
-    response.sendFile(__dirname + "/index.html");
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname +"/index.html");
 });
 
-app.post("/" , function(request , response){
-    response.send("thanks for posting that");
+app.post("/", function(req, res){
+   
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var result = num1 + num2 ;
+
+    res.send("result of calculation is " + result);
 });
-
-
 
 app.listen(3000 , function(){console.log("server started on port 3000");});
